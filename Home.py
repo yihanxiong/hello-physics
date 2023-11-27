635604c40c946162cb31dcb74a2083401bafcb6f
Yihan Xiong <146686992+yihanxiong@users.noreply.github.com>
2023-11-26 22:10:37 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -29,16 +29,7 @@ with col2:
 #st.header('Hello, I am Youti Franklin Wan :wave:', divider='rainbow')
 #st.markdown(
     """
-    ### I am a high school student from [the Lawrenceville School](https://www.lawrenceville.org). I am here to share some of my Physics Simulations!
-    ### This website is made with Streamlit and Python. 
-    ### **👈 Select a demo from the sidebar** to see some fundamental physics simulations. 
-    ### Why to show these simulations?
-    - adefd
-    - feefd
-    - efewfefw
-    ### What I learn from these simulations?
-    - abced
-    - Efegdd
+   coming soon
 """
 #)
 
